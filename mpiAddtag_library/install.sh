9b91856a989707552f6e436a81cbc5d54daf378a
simapaso3652 <kokopaso_3652@yahoo.co.jp>
2019-01-25 01:39:12 +0900

    rm mpiAddtag_library/addTag_INPacket.c change install.sh. it is corrct shell

--- mpiAddtag_library/install.sh
+++ mpiAddtag_library/install.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 
 echo "install.sh ----------"
+
+#check.sh
 ./check.sh
 
-askYesOrNo() {
-    while true ; do
-        read -p "$1 (y/n)?" answer
-        case $answer in
-            [yY] | [yY]es | YES )
-                return 0;;
-            [nN] | [nN]o | NO )
-                return 1;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
+# touch result.txt
+echo "touch result.txt at libsource"
+touch ./libsource/result.txt
 
-
-# MPIライブラリの置換
-# ./extendMPILibrary.sh
-
-# LSM_MPIAddtag のインストール
-echo "meke menuconfigを実行します。システムを起動させたい場合には、LSM MPI addtagを選択してください。"
-sudo make menuconfig
-
-# コンパイル
-#make security/
-#make modules_install
+echo "init.sh end-----------------------------"
