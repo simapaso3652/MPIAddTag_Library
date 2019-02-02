2fc410521ec0884c6fee8482558320e8a0322649
simapaso3652 <kokopaso_3652@yahoo.co.jp>
2019-02-02 20:08:30 +0900

    add function install.sh, systemOn.sh

--- mpiAddtag_library/check.sh
+++ mpiAddtag_library/check.sh
@@ -4,18 +4,26 @@
 echo "check requested library"
 # mpi
 mpirun > /dev/null 2>&1
-#if [ $? -eq 127 ]; then
-  # コマンドをインストールする処理とか
-  #echo "you need to install mpi before init.sh"
-  #return 1
+if [ $? -eq 127 ]; then
+  コマンドをインストールする処理とか
+  echo "you need to install mpi before init.sh"
+  return 1
   echo 
-#fi
+fi
+# sshpass
+if rpm -qa sshpass | grep -q sshpass; then
+  true
+else
+  echo "please install sshpass before init.sh"
+  return 1
+fi
 # kernel-devel
-#if [ $? -eq 127 ]; then
-  # コマンドをインストールする処理とか
- # echo "you need to install kernel-devel before init.sh"
- # return 1
-#fi
+if rpm -qa kernel-devel | grep -q kernel-devel; then
+  true
+else
+  echo "please installkernel-devel before init.sh"
+  return 1
+fi
 
 echo "checkok!!"
 echo ""
--- mpiAddtag_library/install.sh
+++ mpiAddtag_library/install.sh
@@ -10,14 +10,18 @@ if [ path != '' ]; then
   echo "please type your mpiLibrary's path
   read path
 fi
-# mv ./libsource/mpi_send.c path/~
+# mv ./libsource/mpi_send.c $path/~ ######
 
 # MPIライブラリの再ビルドを行います。
 echo "rebuild mpiLibrary for adaping of mpiLibrary"
+sudo $path/configure --prefix=/usr/local/openmpi-2.0.1  CC=gcc CXX=g++ 
+make -j 4 all 2>&1 | tee make.log
+make -j 4 install 2>&1 | tee install.log
 
 # 識別カーネルモジュールタグ付けプログラムのコンパイル
-echo "compile mpiAddTag_INPacket.c. It is identification kernel module"
-gcc -c -o mpiAddTag_INPacket.o
+echo "compile mpiAddTag_INPacket.c. It is one program, kernel module for addTag"
+gcc -c ./libsource/mpiAddTag_INPacket.c -o mpiAddTag_INPacket.o
+
 
 echo "install.sh end-----------------------------"
 echo ""
